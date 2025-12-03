d7faa4aebf1fb33d44e827e193619196ebb56899
L4ve <pawelskowronski2008@gmail.com>
2025-12-03 12:48:32 +0100

    Dodaj pliki projektów.

---
+++ MyszkaMauii/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyszkaMauii
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyszkaMauii/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MyszkaMauii
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnLinkTapped(object sender, EventArgs e)
+        {
+            if (sender is Label label &&
+                (label.GestureRecognizers.FirstOrDefault() as TapGestureRecognizer) is TapGestureRecognizer tap &&
+                tap.CommandParameter is string url)
+            {
+                await Launcher.OpenAsync(url);
+            }
+        }
+    }
+}
