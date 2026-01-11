8ee83906d8b3d656bce6dffcb3a614f9ff386537
L4ve <pawelskowronski2008@gmail.com>
2026-01-11 22:36:44 +0100

    teraz wkońcu działa na api, nie na czysty db sqlite, dodano komentarze, i chyba wszystko jest git

--- MyszkaMauii/MainPage.xaml.cs
+++ MyszkaMauii/MainPage.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Maui.Controls;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace MyszkaMauii
@@ -9,92 +8,107 @@ namespace MyszkaMauii
     public partial class MainPage : ContentPage
     {
         private List<Mouse1> mice;
-        //sortowanie
+
+        // do sortowania
         private bool sortAscending = true;
 
-        //bierze rzeczy z bazy danych (MouseDatabase.cs)
-        private MouseDatabase _db;
+        // API
+        private MouseApiService _api;
+
+        // Referencja do tabeli
+        private Grid miceGrid;
 
         public MainPage()
         {
-            InitializeComponent();
+            InitializeComponent(); 
 
-            var dbPath = Path.Combine(FileSystem.AppDataDirectory, "mice.db3");
-            _db = new MouseDatabase(dbPath);
-
-            LoadMiceFromDb();
+            _api = new MouseApiService(); // instancja api
+            LoadMiceFromApi(); // pobiera dane z api przy starcie
         }
-        // bierze dane z bazy
-        private async void LoadMiceFromDb()
+
+        // pobieranie myszy z api
+        private async void LoadMiceFromApi()
         {
-            mice = await _db.GetMiceAsync();
-
-            DisplayMice(mice);
+            mice = await _api.GetMiceAsync();
+            DisplayMice(mice); // wyświetlenie myszy w tabeli
         }
 
+        // wyświetlanie tabeli
         private void DisplayMice(List<Mouse1> miceToDisplay)
         {
-            //stworzenie grida dla myszek
-            var grid = new Grid
+            // usuń starą tabelę, jeśli istnieje, aby sie nie kopiowała bo stworzeniu nowej myszy
+            if (miceGrid != null)
+            {
+                MyStackLayout.Children.Remove(miceGrid);
+            }
+
+            miceGrid = new Grid
             {
                 ColumnDefinitions =
                 {
-                    new ColumnDefinition { Width = 50 },
+                    new ColumnDefinition { Width = 50 }, 
                     new ColumnDefinition { Width = 150 },
-                    new ColumnDefinition { Width = 120 },
-                    new ColumnDefinition { Width = 120 },
-                    new ColumnDefinition { Width = 80 },
-                    new ColumnDefinition { Width = GridLength.Star } // miałem problem z czarnym pudłem w kodzie, to jest dodane tylko po to by tego nie pokazywało
+                    new ColumnDefinition { Width = 120 },  
+                    new ColumnDefinition { Width = 120 },  
+                    new ColumnDefinition { Width = 80 },    
+                    new ColumnDefinition { Width = GridLength.Star }// było czarne pole bez tego jest to użyte jako filler
                 },
                 RowSpacing = 1,
-                ColumnSpacing = 1,
+                ColumnSpacing = 1
             };
 
-            // Nagłówki
-            grid.RowDefinitions.Add(new RowDefinition { Height = 40 });
-            grid.Children.Add(CreateHeaderLabel("Id", 0, 0));
-            grid.Children.Add(CreateHeaderLabel("Model", 0, 1));
-            grid.Children.Add(CreateHeaderLabel("Firma", 0, 2));
-            grid.Children.Add(CreateHeaderLabel("Sensor", 0, 3));
-            grid.Children.Add(CreateHeaderLabel("Waga", 0, 4, true)); // po kliknięciu w "waga" sortuje myszki wagowo
-            grid.Children.Add(CreateHeaderLabel("Link", 0, 5));
+            miceGrid.RowDefinitions.Add(new RowDefinition { Height = 40 });
+            miceGrid.Children.Add(CreateHeaderLabel("Id", 0, 0));
+            miceGrid.Children.Add(CreateHeaderLabel("Model", 0, 1));
+            miceGrid.Children.Add(CreateHeaderLabel("Firma", 0, 2));
+            miceGrid.Children.Add(CreateHeaderLabel("Sensor", 0, 3));
+            miceGrid.Children.Add(CreateHeaderLabel("Waga", 0, 4, true)); // Klikany do sortowania
+            miceGrid.Children.Add(CreateHeaderLabel("Link", 0, 5));
 
             int rowIndex = 1;
+
+            // wypełnianie tabeli
             foreach (var m in miceToDisplay)
             {
-                grid.RowDefinitions.Add(new RowDefinition { Height = 40 });
+                miceGrid.RowDefinitions.Add(new RowDefinition { Height = 40 });
 
-                //zamienia kolory z jednego na drugi co drugi row z białego na lightgray
-                var rowBackground = rowIndex % 2 == 0 ? Colors.White : Colors.LightGray;
+                // kolorowanie wierszy na przemian(to coś jest z chata bo wyglądało brzydko z jednym kolorem)
+                var bg = rowIndex % 2 == 0 ? Colors.White : Colors.LightGray;
 
-                grid.Children.Add(CreateCellLabel(m.Id.ToString(), rowIndex, 0, rowBackground));
-                grid.Children.Add(CreateCellLabel(m.Model, rowIndex, 1, rowBackground));
-                grid.Children.Add(CreateCellLabel(m.Firma, rowIndex, 2, rowBackground));
-                grid.Children.Add(CreateCellLabel(m.Sensor, rowIndex, 3, rowBackground));
-                grid.Children.Add(CreateCellLabel(m.Waga.ToString(), rowIndex, 4, rowBackground));
-                //Label jako link
+                // dodanie komórek do tabeli
+                miceGrid.Children.Add(CreateCellLabel(m.Id.ToString(), rowIndex, 0, bg));
+                miceGrid.Children.Add(CreateCellLabel(m.Model, rowIndex, 1, bg));
+                miceGrid.Children.Add(CreateCellLabel(m.Firma, rowIndex, 2, bg));
+                miceGrid.Children.Add(CreateCellLabel(m.Sensor, rowIndex, 3, bg));
+                miceGrid.Children.Add(CreateCellLabel(m.Waga.ToString(), rowIndex, 4, bg));
+
+                // funkcja linku
                 var linkLabel = new Label
                 {
                     Text = "Otwórz link",
-                    TextColor = Colors.Blue,
-                    TextDecorations = TextDecorations.Underline,
-                    BackgroundColor = rowBackground,
+                    TextColor = Colors.Black,
+                    BackgroundColor = bg,
                     HorizontalTextAlignment = TextAlignment.Center,
-                    VerticalTextAlignment = TextAlignment.Center
+                    VerticalTextAlignment = TextAlignment.Center,
+                    TextDecorations = TextDecorations.Underline
                 };
+
+                // działanie kliknięcia w link
                 var tap = new TapGestureRecognizer { CommandParameter = m.Link };
                 tap.Tapped += OnLinkTapped;
                 linkLabel.GestureRecognizers.Add(tap);
+
                 Grid.SetRow(linkLabel, rowIndex);
                 Grid.SetColumn(linkLabel, 5);
-                grid.Children.Add(linkLabel);
+                miceGrid.Children.Add(linkLabel);
 
                 rowIndex++;
             }
-            //dodanie tabeli do stacklayout
-            MyStackLayout.Children.Add(grid);
+
+            MyStackLayout.Children.Add(miceGrid);
         }
-        //tworzenie headerów
+
+        // nagłówki
         private Label CreateHeaderLabel(string text, int row, int column, bool isClickable = false)
         {
             var label = new Label
@@ -106,18 +120,20 @@ namespace MyszkaMauii
                 VerticalTextAlignment = TextAlignment.Center,
                 FontAttributes = FontAttributes.Bold
             };
+
             Grid.SetRow(label, row);
             Grid.SetColumn(label, column);
-            // kod do klikania wagi dla sortowania
+
+            // waga jest do kliknięcia, wtedy sortuje
             if (isClickable)
             {
                 var tap = new TapGestureRecognizer();
                 tap.Tapped += (s, e) =>
                 {
                     mice = sortAscending
-                        ? mice.OrderBy(m => m.Waga).ToList()
-                        : mice.OrderByDescending(m => m.Waga).ToList();
-                    sortAscending = !sortAscending;
+                        ? mice.OrderBy(m => m.Waga).ToList() : mice.OrderByDescending(m => m.Waga).ToList();
+
+                    sortAscending = !sortAscending; // można klikać do woli i działa
                     DisplayMice(mice); // odświeżenie tabeli
                 };
                 label.GestureRecognizers.Add(tap);
@@ -126,6 +142,7 @@ namespace MyszkaMauii
             return label;
         }
 
+        // komórki
         private Label CreateCellLabel(string text, int row, int column, Color background)
         {
             var label = new Label
@@ -136,28 +153,32 @@ namespace MyszkaMauii
                 HorizontalTextAlignment = TextAlignment.Center,
                 VerticalTextAlignment = TextAlignment.Center
             };
+
             Grid.SetRow(label, row);
             Grid.SetColumn(label, column);
             return label;
         }
-        // funkcja otwierania linku w przeglądarce(zabrałem to z neta nie mam pojęcia co to robi)
+
+        // klikanie w link
         private async void OnLinkTapped(object sender, EventArgs e)
         {
             if (sender is Label label &&
-                (label.GestureRecognizers.FirstOrDefault() as TapGestureRecognizer) is TapGestureRecognizer tap &&
+                label.GestureRecognizers.FirstOrDefault() is TapGestureRecognizer tap &&
                 tap.CommandParameter is string url)
             {
+                // Otwieranie linku w przeglądarce
                 await Launcher.OpenAsync(url);
             }
         }
-        // dodawanie nowej myszy
+
+        // funkcja dodawania nowej myszy
         private async void OnAddMouseClicked(object sender, EventArgs e)
-        {   //sprawdzanie czy waga to liczba
+        {
+            // sprawdza czy waga to liczba
             if (!double.TryParse(WagaEntry.Text, out double waga))
-            {
                 return;
-            }
-            //nowa mysz na podstawie danych z strony
+
+            // dane nowej myszy
             var newMouse = new Mouse1
             {
                 Model = ModelEntry.Text ?? "",
@@ -167,11 +188,15 @@ namespace MyszkaMauii
                 Link = LinkEntry.Text ?? ""
             };
 
-            await _db.SaveMouseAsync(newMouse); //dodanie do bazy danych (mouse.cs)
+                // wywołanie api dodające mysz
+                var addedMouse = await _api.AddMouseAsync(newMouse);
 
-            mice.Add(newMouse); //dodanie do tabeli
-            DisplayMice(mice); //resetowanie tabeli by sie wyświetlało
-            // usuwanie danych po wpisaniu
+                mice.Add(addedMouse); // dodajemy do lokalnej listy
+                DisplayMice(mice); // odświeżamy tabelę
+
+
+
+            // czysci formularz po wysłaniu do api
             ModelEntry.Text = "";
             FirmaEntry.Text = "";
             SensorEntry.Text = "";
---
+++ MyszkaMauii/MouseAPIService.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Json;
+
+namespace MyszkaMauii
+{
+    public class MouseApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public MouseApiService()
+        {
+            _httpClient = new HttpClient
+            {
+                // localhost do skopiowania z api/properties/launchsettings.json
+                BaseAddress = new Uri("http://localhost:5231/api/")
+            };
+        }
+
+        public async Task<List<Mouse1>> GetMiceAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Mouse1>>("mice")
+                   ?? new List<Mouse1>();
+        }
+
+        public async Task<Mouse1> AddMouseAsync(Mouse1 mouse)
+        {
+            var response = await _httpClient.PostAsJsonAsync("mice", mouse);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = await response.Content.ReadAsStringAsync();
+                throw new Exception(error);
+            }
+
+            // api zwraca nową mysz z id
+            return await response.Content.ReadFromJsonAsync<Mouse1>();
+        }
+    }
+}
